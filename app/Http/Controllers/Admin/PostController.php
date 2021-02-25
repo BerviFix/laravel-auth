14044a540ecf7ecc88cb6713cef17a72d2409308
Simone <simone.bervi@gmail.com>
2021-02-25 19:00:28 +0100

    Fix index mostra i post relativi all'utente loggato

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::where('user_id', Auth::id())->get();
 
         return view('admin.posts.index', compact('posts'));
     }
